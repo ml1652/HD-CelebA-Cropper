eb822ac5969b58b1f4617d191ab1f5cd8cb5c493
ml1652 <33670846+ml1652@users.noreply.github.com>
2021-04-13 17:32:42 +0100

    update
    use align_2 .py to crop face image

--- align_2.py
+++ align_2.py
@@ -3,10 +3,13 @@ from __future__ import division
 from __future__ import print_function
 
 import argparse
+import traceback
 from functools import partial
 from multiprocessing import Pool
 import os
 import re
+
+import dlib
 
 import cropper
 import numpy as np
@@ -19,8 +22,9 @@ import tqdm
 
 parser = argparse.ArgumentParser()
 # main
-parser.add_argument('--img_dir', dest='img_dir', default='./data/img_celeba')
-parser.add_argument('--save_dir', dest='save_dir', default='./data/aligned')
+#parser.add_argument('--img_dir', dest='img_dir', default='./data/img_celeba')
+parser.add_argument('--img_dir', dest='img_dir', default= r"C:\Users\Mingrui\Desktop\Github\pytorch_stylegan_encoder\alignment_image_for_test")
+parser.add_argument('--save_dir', dest='save_dir', default= r"C:\Users\Mingrui\Desktop\datasets\StyleGANimge_corp\webimage_alignmentTest")
 parser.add_argument('--landmark_file', dest='landmark_file', default='./data/landmark.txt')
 parser.add_argument('--standard_landmark_file', dest='standard_landmark_file', default='./data/standard_landmark_68pts.txt')
 parser.add_argument('--crop_size_h', dest='crop_size_h', type=int, default=572)
@@ -30,12 +34,14 @@ parser.add_argument('--move_w', dest='move_w', type=float, default=0.)
 parser.add_argument('--save_format', dest='save_format', choices=['jpg', 'png'], default='jpg')
 parser.add_argument('--n_worker', dest='n_worker', type=int, default=4)
 # others
-parser.add_argument('--face_factor', dest='face_factor', type=float, help='The factor of face area relative to the output image.', default=0.45) #default = 0.5
+parser.add_argument('--face_factor', dest='face_factor', type=float, help='The facto r of face area relative to the output image.', default=0.45) #default = 0.5
 parser.add_argument('--align_type', dest='align_type', choices=['affine', 'similarity'], default='similarity')
 parser.add_argument('--order', dest='order', type=int, choices=[0, 1, 2, 3, 4, 5], help='The order of interpolation.', default=3)
 parser.add_argument('--mode', dest='mode', choices=['constant', 'edge', 'symmetric', 'reflect', 'wrap'], default='edge')
 args = parser.parse_args()
 
+ignore_landmark = True
+draw_landmark = False
 def draw_face_landmark(name, landmarkToPaint, img_crop):
     # draw face's landmark
     point_size = 1
@@ -60,19 +66,11 @@ def draw_face_landmark(name, landmarkToPaint, img_crop):
 # ==============================================================================
 
 _DEAFAULT_JPG_QUALITY = 95
-try:
-    import cv2
-    imread = cv2.imread
-    imwrite = partial(cv2.imwrite, params=[int(cv2.IMWRITE_JPEG_QUALITY), _DEAFAULT_JPG_QUALITY])
-    align_crop = cropper.align_crop_opencv
-    print('Use OpenCV')
-except:
-    import skimage.io as io
-    imread = io.imread
-    imwrite = partial(io.imsave, quality=_DEAFAULT_JPG_QUALITY)
-    align_crop = cropper.align_crop_skimage
-    print('Importing OpenCv fails. Use scikit-image')
-
+import cv2
+imread = cv2.imread
+imwrite = partial(cv2.imwrite, params=[int(cv2.IMWRITE_JPEG_QUALITY), _DEAFAULT_JPG_QUALITY])
+align_crop = cropper.align_crop_opencv
+print('Use OpenCV')
 
 # ==============================================================================
 # =                                     run                                    =
@@ -84,8 +82,9 @@ with open(args.landmark_file) as f:
 n_landmark = len(re.split('[ ]+', line)[1:]) // 2
 
 # read data
-img_names = np.genfromtxt(args.landmark_file, dtype=np.str, usecols=0)
+img_names = os.listdir(args.img_dir)
 landmarks = np.genfromtxt(args.landmark_file, dtype=np.float, usecols=range(1, n_landmark * 2 + 1)).reshape(-1, n_landmark, 2)
+
 standard_landmark = np.genfromtxt(args.standard_landmark_file, dtype=np.float).reshape(n_landmark, 2)
 standard_landmark[:, 0] += args.move_w
 standard_landmark[:, 1] += args.move_h
@@ -101,13 +100,61 @@ celeba_standard_landmark = np.loadtxt(r"C:\Users\Mingrui\Desktop\celeba\Anno\sta
 celeba_landmark = np.genfromtxt(r"C:\Users\Mingrui\Desktop\celeba\Anno\list_landmarks_celeba.txt", dtype=np.float,usecols = range(1, 5 * 2 + 1), skip_header = 2).reshape(-1, 5, 2)
 
 
+def generate_landmark(img):
+    # location of the model (path of the model).
+    Model_PATH = r"C:\Users\Mingrui\Desktop\Github\pytorch_stylegan_encoder\models\shape_predictor_68_face_landmarks.dat"
+
+    # now from the dlib we are extracting the method get_frontal_face_detector()
+    # and assign that object result to frontalFaceDetector to detect face from the image with
+    # the help of the 68_face_landmarks.dat model
+    frontalFaceDetector = dlib.get_frontal_face_detector()
+
+    imageRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+    # Now the dlip shape_predictor class will take model and with the help of that, it will show
+    faceLandmarkDetector = dlib.shape_predictor(Model_PATH)
+
+    # Now this line will try to detect all faces in an image either 1 or 2 or more faces
+    allFaces = frontalFaceDetector(imageRGB, 0)
+
+    # List to store landmarks of all detected faces
+    allFacesLandmark = []
+
+    # Below loop we will use to detect all faces one by one and apply landmarks on them
+
+    for k in range(0, max(1, len(allFaces))):
+        # dlib rectangle class will detecting face so that landmark can apply inside of that area
+        faceRectangleDlib = dlib.rectangle(int(allFaces[k].left()), int(allFaces[k].top()),
+                                           int(allFaces[k].right()), int(allFaces[k].bottom()))
+
+        # Now we are running loop on every detected face and putting landmark on that with the help of faceLandmarkDetector
+        detectedLandmarks = faceLandmarkDetector(imageRGB, faceRectangleDlib)
+
+        # count number of landmarks we actually detected on image
+        # if k == 0:
+        #     print("Total number of face landmarks detected ", len(detectedLandmarks.parts()))
+
+        # Svaing the landmark one by one to the output folder
+        for point in detectedLandmarks.parts():
+            allFacesLandmark.append([point.x, point.y])
+            if draw_landmark:
+                img = cv2.circle(img, (point.x, point.y), 2, (0, 0, 255), 3)
+                cv2.imshow("preview", img)
+                cv2.waitKey(0)
+                cv2.destroyAllWindows()
+
+
+    return np.array(allFacesLandmark)
+
 def work(i):  # a single work
     for _ in range(3):  # try three times
         try:
             img = imread(os.path.join(args.img_dir, img_names[i]))
-            lm = landmarks[0]
-            img_crop, tformed_landmarks, tformed_celeba_landmarks = align_crop(img,
-                                                     landmarks[i],
+
+            img_landmark = generate_landmark(img) if ignore_landmark else landmarks[i]
+
+            img_crop, tformed_landmarks, tformed_celeba_landmarks,tform = align_crop(img,
+                                                     img_landmark,
                                                      standard_landmark,
                                                      celeba_standard_landmark,
                                                      celeba_landmark[i],
@@ -156,7 +203,7 @@ def work(i):  # a single work
             break
         except Exception as e:
             succeed = False
-            print(e.__traceback__)
+            print(e)
     if succeed:
         return name_landmark_str, ' '.join([name] + [str(int(x)) for x in tformed_celeba_landmarks])
     else:
@@ -164,29 +211,17 @@ def work(i):  # a single work
 
 
 if __name__ == '__main__':
-
-    pool = Pool(args.n_worker)
-
-    values = list(tqdm.tqdm(pool.imap(work, range(len(img_names))), total=len(img_names)))
     name_landmark_strs = []
     str2 = []
-    for x in values:
-        name_landmark_strs.append(x[0])
-        str2.append(x[1])
 
-    pool.close()
-    pool.join()
+    for i in range(len(img_names)):
+        result = work(i)
+        if result is None:
+            print(img_names[i] + ' skipped.')
+            continue
 
-    #np.savetxt("tformed_landmarks.txt", tformed_landmarks);
-    landmarks_path = os.path.join(save_dir, 'tformed_landmark_68point.txt')
-    with open(landmarks_path, 'w') as f:
-        for name_landmark_str in name_landmark_strs:
-            if name_landmark_str:
-                f.write(name_landmark_str + '\n')
+        (a, b) = result
+        name_landmark_strs.append(a)
+        str2.append(b)
 
-    #np.savetxt("tformed_landmarks.txt", tformed_landmarks);
-    landmarks_path = os.path.join(save_dir, 'tformed_landmark_5point.txt')
-    with open(landmarks_path, 'w') as f:
-        for name_landmark_str in str2:
-            if name_landmark_str:
-                f.write(name_landmark_str + '\n')
+
--- cropper.py
+++ cropper.py
@@ -1,9 +1,11 @@
 import numpy as np
-
+from numpy import linalg as LA
 
 def align_crop_opencv(img,
                       src_landmarks,
                       standard_landmarks,
+                      celeba_standard_landmark,
+                      src_celeba_landmark,
                       crop_size=512,
                       face_factor=0.7,
                       align_type='similarity',
@@ -32,6 +34,7 @@ def align_crop_opencv(img,
     """
     # set OpenCV
     import cv2
+
     inter = {0: cv2.INTER_NEAREST, 1: cv2.INTER_LINEAR, 2: cv2.INTER_AREA,
              3: cv2.INTER_CUBIC, 4: cv2.INTER_LANCZOS4, 5: cv2.INTER_LANCZOS4}
     border = {'constant': cv2.BORDER_CONSTANT, 'edge': cv2.BORDER_REPLICATE,
@@ -52,21 +55,46 @@ def align_crop_opencv(img,
     else:
         raise Exception('Invalid `crop_size`! `crop_size` should be 1. int for (crop_size, crop_size) or 2. (int, int) for (crop_size_h, crop_size_w)!')
 
+
+
     # estimate transform matrix
     trg_landmarks = standard_landmarks * max(crop_size_h, crop_size_w) * face_factor + np.array([crop_size_w // 2, crop_size_h // 2])
+
     if align_type == 'affine':
         tform = cv2.estimateAffine2D(trg_landmarks, src_landmarks, ransacReprojThreshold=np.Inf)[0]
     else:
         tform = cv2.estimateAffinePartial2D(trg_landmarks, src_landmarks, ransacReprojThreshold=np.Inf)[0]
 
+    #calcaute the scale of tform
+    m1 = np.mat('0;0;1')
+    m2 = np.mat('1;0;1')
+    p1 = tform.dot(m1)
+    p2 = tform.dot(m2)
+    scale = LA.norm(p2 - p1) # defualt is Frobenius norm
+
+    # change the translations part of the transformation matrix for downwarding vertically
+    tform[1][2] = tform[1][2] + 20*scale
+
+
     # warp image by given transform
     output_shape = (crop_size_h, crop_size_w)
     img_crop = cv2.warpAffine(img, tform, output_shape[::-1], flags=cv2.WARP_INVERSE_MAP + inter[order], borderMode=border[mode])
 
+    # #center crop
+    # center_crop_size = 224
+    # mid_x, mid_y = int(crop_size_w / 2), int(crop_size_h / 2)
+    # mid_y = mid_y +16
+    # cw2, ch2 = int(center_crop_size / 2), int(center_crop_size / 2)
+    # img_crop = img_crop[mid_y-ch2:mid_y+ch2, mid_x-cw2:mid_x+cw2]
+
+
+
+
     # get transformed landmarks
     tformed_landmarks = cv2.transform(np.expand_dims(src_landmarks, axis=0), cv2.invertAffineTransform(tform))[0]
+    tformed_celeba_landmarks = cv2.transform(np.expand_dims(src_celeba_landmark, axis=0), cv2.invertAffineTransform(tform))[0]
 
-    return img_crop, tformed_landmarks
+    return img_crop, tformed_landmarks,tformed_celeba_landmarks,tform
 
 
 def align_crop_skimage(img,
@@ -99,3 +127,31 @@ def align_crop_skimage(img,
                              to the given mode.
     """
     raise NotImplementedError("'align_crop_skimage' is not implemented!")
+
+#https://en.wikipedia.org/wiki/Procrustes_analysis
+def transformation_from_points(points1, points2):
+    points1 = points1.astype(np.float64)
+    points2 = points2.astype(np.float64)
+
+    c1 = np.mean(points1, axis=0)
+    c2 = np.mean(points2, axis=0)
+    points1 -= c1
+    points2 -= c2
+
+    s1 = np.std(points1)
+    s2 = np.std(points2)
+    points1 /= s1
+    points2 /= s2
+
+    # U, S, Vt = np.linalg.svd(points1.T * points2)
+    # R = (U * Vt).T
+
+    # return np.vstack([np.hstack(((s2 / s1) * R,
+    #                                    c2.T - (s2 / s1) * R * c1.T)),np.matrix([0., 0., 1.])])
+    return np.around(points1,5), np.around(points2,5)
+
+landmarks = np.genfromtxt(r"C:\Users\Mingrui\Desktop\celeba\Anno\list_landmarks_celeba.txt", usecols=range(1, 5 * 2 + 1),skip_header = 2).reshape(-1, 5, 2)
+x_celeba_lanamrk = landmarks[:,:,0]
+y_celeba_lanamrk = landmarks[:,:,1]
+transformation_from_points(x_celeba_lanamrk,y_celeba_lanamrk)
+
